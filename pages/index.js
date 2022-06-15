01ea8417e2428280c12a89b6a491998f6efa38ce
Sachin Sharma <sachin@gryt.fit>
2022-06-16 01:48:06 +0530

    ADD: new meta tags

--- pages/index.js
+++ pages/index.js
@@ -12,26 +12,26 @@ export default function Home() {
         <meta name="theme-color" content="#000000" />
         <meta name="robots" content="index, follow" />
         <meta name="google-site-verification" content="4wMoS5YqfF0DyNHr--nI4Z5iWUdka8KPYiBiCQGv5EA" />
-        <title>Unberry : Transforming the future of hiring and developing talent.</title>
+        <title>Unberry - Transforming the future of hiring and developing talent.</title>
+        <meta name="name" content="Unberry - Transforming the future of hiring and developing talent."/>
         <meta name="description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
 
         {/* OG Section  */}
-        <meta property="og:url" content="https://unberry.com/static/og.png" />
-        <meta property="og:site_name" content="unberry.com" />
         <meta property="og:type" content="website" />
-        <meta property="fb:app_id" content="546555445835110" />
-        <meta property="og:image" content="https://unberry.com/static/og.png" />
-        <meta name="og:title" content="Transforming the future of hiring and developing talent." />
+        <meta property="og:site_name" content="unberry.com" />
+        <meta name="og:title" content="Unberry - Transforming the future of hiring and developing talent." />
         <meta name="og:description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
+        <meta property="og:url" content="https://unberry.com/static/og.png" />
+        <meta property="og:image" content="https://unberry.com/static/og.png" />
+        <meta property="og:image:alt" content="Unberry - Transforming the future of hiring and developing talent."/>
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
         {/* Twitter OG Section  */}
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="Twitter @unberry.com" />
-        <meta name="twitter:title" content="Transforming the future of hiring and developing talent." />
+        <meta name="twitter:title" content="Unberry - Transforming the future of hiring and developing talent." />
         <meta name="twitter:description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
-        <meta name="twitter:image" content="twitter OG image image path here" />
         <meta name="twitter:image" content="https://unberry.com/static/og.png" />
 
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-303682441"></script>
