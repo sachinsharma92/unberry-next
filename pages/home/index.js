458fdb4fdee4c78a6a7b9584ceb20ea8c2161092
Sachin Sharma <sachin@gryt.fit>
2022-06-16 21:41:21 +0530

    FIX: text issues

--- pages/home/index.js
+++ pages/home/index.js
@@ -132,11 +132,6 @@ export default function HomeScreen() {
 
   return (
     <div className='home-section-style'>
-      <Head>
-        <title>Unberry - Transforming the future of hiring and developing talent.</title>
-        <meta name="title" content="Unberry - Transforming the future of hiring and developing talent." />
-        <meta name="description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
-      </Head>
       <LayoutPrimary footer>
         <div className='mobile-header'>
           <Link href="/">
