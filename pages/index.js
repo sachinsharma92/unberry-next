e8072a60534383c27ce6a5de890ad2d63f7f3de0
Sachin Sharma <sachin@gryt.fit>
2022-06-18 18:02:39 +0530

    FIX: some content issues

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,23 @@ export default function Home() {
         <meta name="google-site-verification" content="4wMoS5YqfF0DyNHr--nI4Z5iWUdka8KPYiBiCQGv5EA" />
 
         {/* Main Title */}
-        <title>Unberry - Transforming the future of hiring and developing talent...</title>
+        <title>Unberry - Transforming the future of hiring and developing talent!</title>
         <meta name="title" content="Unberry - Transforming the future of hiring and developing talent."/>
         <meta name="description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
         <meta name="image" content="https://unberry.com/static/og.png" />
 
         {/* OG Section  */}
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="unberry.com" />
-        <meta name="og:title" property="og:title" content="Unberry - Transforming the future of hiring and developing talent." />
+        {/* <meta property="og:site_name" content="unberry.com" /> */}
+        <meta property="og:title" content="Unberry - Transforming the future of hiring and developing talent." />
         <meta property="og:url" content="https://unberry.com/static/og.png" />
-        <meta name="og:description" property="og:description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
+        <meta property="og:description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" />
         <meta property="og:image" content="https://unberry.com/static/og.png" />
-        <meta property="og:image:url" content="https://unberry.com/static/og.png" />
-        <meta property="og:image:alt" content="Unberry - Transforming the future of hiring and developing talent."/>
+        {/* <meta property="og:image:url" content="https://unberry.com/static/og.png" /> */}
+        {/* <meta property="og:image:alt" content="Unberry - Transforming the future of hiring and developing talent."/>
         <meta property="og:image:type" content="website" />
         <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:image:height" content="630" /> */}
 
         {/* Twitter OG Section  */}
         <meta name="twitter:card" content="summary" />
