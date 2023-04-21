42dac4ea554b71e0e43c46f791f63335a46f0a9d
sachinsharma92 <sachindesigner2018@gmail.com>
2023-04-21 17:35:09 +0530

    FIX: title

---
+++ pages/homepage/index.js
@@ -0,0 +1,46 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import HeroSection from '../../components/homepage/heroSection';
+
+// Images
+import Head from 'next/head';
+
+// Styles here
+import styles from './style.module.scss';
+
+
+export default function HomePageScreen() {
+
+  return (
+    <>
+      <Head>
+        <title>Unberry - Transforming the future of hiring and developing talent.</title>
+        <meta name="title" property="og:title" content="Unberry - Transforming the future of hiring and developing talent." />
+        <meta name="description" content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent" key="desc" />
+        <meta
+          property="og:description"
+          content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent."
+        />
+        <meta property="og:url" content="https://unberry.com/static/ogImage.png" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="546555445835110" />
+        <meta property="og:image" content="https://unberry.com/static/ogImage.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="author" content="Unberry" />
+        <meta
+          property="og:image"
+          content="https://unberry.com/static/ogImage.png"
+        />
+
+        {/* Twitter OG Section  */}
+        <meta name="twitter:image" content="https://unberry.com/static/ogImage.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <div className={styles.homepageStyle}>
+        <HeroSection />
+      </div>
+    </>
+  )
+}
