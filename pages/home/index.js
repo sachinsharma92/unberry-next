690bb89c56edb4d967598f499837e37f4e0a7299
sachinsharma92 <sachindesigner2018@gmail.com>
2023-04-21 15:09:01 +0530

    CHANGE: title

--- pages/home/index.js
+++ pages/home/index.js
@@ -60,12 +60,27 @@ export default function HomeScreen() {
         <meta property="og:title" content="Unberry - Transforming the future of hiring and developing talent." />
         <meta
           property="og:description"
-          content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent"
+          content="Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retain and delelop talent."
         />
+
+        <meta property="og:url" content="https://unberry.com/static/ogImage.png" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="546555445835110" />
+        <meta property="og:image" content="https://unberry.com/static/ogImage.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="author" content="Unberry" />
         <meta
           property="og:image"
           content="https://unberry.com/static/ogImage.png"
         />
+
+        {/* Twitter OG Section  */}
+        <meta
+          name="twitter:image"
+          content="https://unberry.com/static/ogImage.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <div className='home-section-style'>
