fdd8dd732fae5399a4234c0bc983ae71363163cd
sachinsharma92 <sachindesigner2018@gmail.com>
2023-05-17 23:10:17 +0530

    FIX: video auto play

--- components/homepage/betterTalent/index.js
+++ components/homepage/betterTalent/index.js
@@ -1,14 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
-import React, { useState } from 'react';
+import Image from "next/image";
+import React, { useState } from "react";
 import Modal from "react-responsive-modal";
-import 'react-responsive-modal/styles.css';
+import "react-responsive-modal/styles.css";
 import image1 from "../../../assets/new/across.png";
 import buttonIcon from "../../../assets/new/button.svg";
 
 // Styles here
-import styles from './styles.module.scss';
-
+import styles from "./styles.module.scss";
 
 export default function BetterTalent() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,17 +18,23 @@ export default function BetterTalent() {
 
   return (
     <div className={styles.betterTalentSectionStyle}>
-      <div className='container-width'>
+      <div className="container-width">
         <div className={styles.contentSection}>
           <h1 className={`${styles.title1}`}>
-            We urge companies to make a real effort in how they can <span>see more in people.</span>
+            We urge companies to make a real effort in how they can{" "}
+            <span>see more in people.</span>
           </h1>
         </div>
 
         <div className={styles.imgbox}>
-          <Image src={image1} alt='image services' className={styles.imgStyle} layout="fill" />
+          <Image
+            src={image1}
+            alt="image services"
+            className={styles.imgStyle}
+            layout="fill"
+          />
           <button className={styles.buttonStyle} onClick={modalToggle}>
-            <Image src={buttonIcon} alt='image services' layout="fill" />
+            <Image src={buttonIcon} alt="image services" layout="fill" />
           </button>
         </div>
 
@@ -38,16 +43,26 @@ export default function BetterTalent() {
           onClose={modalToggle}
           center
           classNames={{
-            overlay: 'customOverlay',
-            modal: 'better-talent-modal',
+            overlay: "customOverlay",
+            modal: "better-talent-modal",
           }}
         >
           <div className={styles.videoBox}>
+            <div
+              className="safari_only"
+              dangerouslySetInnerHTML={{
+                __html: `
+              <video className={styles.videoSection} playsinline autoplay loop muted>
+                  <source src="https://assets.unberry.com/media/Unberry-Video.mp4" type="video/mp4"/>
+              </video>`,
+              }}
+            />
+
             <video
-              className={styles.videoSection}
+              className={`${styles.videoSection}`}
               autoPlay
               loop
-              id='team-video'
+              id="team-video"
             >
               <source
                 src="https://assets.unberry.com/media/Unberry-Video.mp4"
@@ -59,5 +74,5 @@ export default function BetterTalent() {
         </Modal>
       </div>
     </div>
-  )
+  );
 }
