21168f3f34e04abeda884743f04a57799df21a6b
sachinsharma92 <sachindesigner2018@gmail.com>
2023-07-25 13:37:20 +0530

    FIX: some major issues

--- components/howItWorks/worksHeroSection/index.js
+++ components/howItWorks/worksHeroSection/index.js
@@ -1,12 +1,72 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import { gsap } from 'gsap';
+import React, { useEffect, useState } from 'react';
+import ImageCustom from "../../../common/ImageCustom";
 import ButtonPrimary from '../../../common/buttonPrimary';
 
 // Styles here
-import ImageCustom from "../../../common/ImageCustom";
 import styles from './styles.module.scss';
 
 export default function WorksHeroSection(props) {
+  const [count, setCount] = useState(5);
+  const [activeTitle, setActiveTitle] = useState(1)
+
+  const dataList = [
+    {
+      imgUrl: "h-1.png",
+      title: "Data",
+      description: "We leverage a vast data network, contextual industry models, and deep learning to empower of dynamic platform.",
+      id: 1,
+    },
+    {
+      imgUrl: "score-1.svg",
+      title: "Intelligence",
+      description: "Dynamic system of intelligence that bridges the gap between people, data, and interactions.",
+      id: 2,
+    },
+    {
+      imgUrl: "score-2.svg",
+      title: "Experience",
+      description: "Integrating AI, automation, and personalisation for extraordinary talent experiences.",
+      id: 3
+    },
+  ];
+
+
+  const [imageSource, setImageSource] = useState(dataList[0].imgUrl);
+
+  useEffect(() => {
+    let ImageSourceInterval;
+    if (count < dataList.length) {
+      ImageSourceInterval = setInterval(() => setCount(count + 1), 2500);
+    }
+    if (count >= dataList.length) {
+      setCount(0);
+    }
+    setImageSource(dataList[count]?.imgUrl);
+    setActiveTitle(dataList[count]?.id)
+    return () => clearInterval(ImageSourceInterval);
+  }, [count]);
+
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(".animation-block .animation-how-work", 1.4, {
+      y: 100,
+      ease: "power4.out",
+      delay: 0.1,
+      skewY: 0,
+      stagger: {
+        amount: 0.3
+      },
+      autoAlpha: 0,
+      scrollTrigger: {
+        trigger: '#howWeWork',
+        start: 'top bottom',
+        markers: false
+      },
+    })
+  }, [])
 
   return (
     <div className={styles.workHeroStyle}>
@@ -29,35 +89,27 @@ export default function WorksHeroSection(props) {
           <div className={styles.timelineSection}>
             <h4 className={styles.titleMedium}>Geared for Rapidity. Optimized for Expansion. <span>Fueled by AI</span></h4>
             <div className={styles.timelineBox}>
-              <div className={styles.imgBox}>
-                <ImageCustom src="h-1.png" alt="Unberry - Know More in a Detailed Video " layout="fill" />
+              {imageSource && (<div className={styles.imgBox}>
+                <ImageCustom
+                  key={imageSource}
+                  src={imageSource}
+                  alt="Unberry"
+                  layout="fill"
+                />
               </div>
-              <div className={styles.contentSection}>
-                <div className={styles.itemStyle}>
-                  <h4 className={`${styles.titleSM} title1`}>
-                    Data
-                  </h4>
-                  <p className={`${styles.description} description`}>
-                    We leverage a vast data network, contextual industry models, and deep learning to empower of dynamic platform.
-                  </p>
-                </div>
-
-                <div className={styles.itemStyle}>
-                  <h4 className={`${styles.titleSM} title1`}>
-                    Intelligence
-                  </h4>
-                  <p className={`${styles.description} description`}>
-                    Dynamic system of intelligence that bridges the gap between people, data, and interactions .
-                  </p>
-                </div>
-
-                <div className={styles.itemStyle}>
-                  <h4 className={`${styles.titleSM} title1`}>
-                    Experience
-                  </h4>
-                  <p className={`${styles.description} description`}>
-                    Integrating AI, automation, and personalisation for extraordinary talent experiences.
-                  </p>
+              )}
+              <div className={`${styles.contentSection} ${styles.contentTimeline}`} id="contentSection">
+                <div>
+                  {dataList.map((data, index) => (
+                    <div key={index} className={`${styles.itemStyle} ${data.id === activeTitle ? styles.active : ''}`} id={`item${index + 1}`}>
+                      <h4 className={`${styles.titleSM} title1`}>
+                        {data.title}
+                      </h4>
+                      <p className={`${styles.description} description`}>
+                        {data.description}
+                      </p>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
