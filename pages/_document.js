fd30b30945d3f3164aa8377982932acf32ca2e26
sachinsharma92 <sachindesigner2018@gmail.com>
2023-11-22 15:07:05 +0530

    REMOVE: lazysizes file

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,42 @@
 /* eslint-disable @next/next/no-title-in-document-head */
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@300;400;500&family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap"
-          rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@300;400;500&family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
 
         {/* Animations JS */}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js" async />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.3/ScrollMagic.js" async />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/animation.gsap.min.js" async />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/debug.addIndicators.min.js" async />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js" async />
-        <script src="lazysizes.min.js" async />
-
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"
+          async
+        />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.3/ScrollMagic.js"
+          async
+        />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/animation.gsap.min.js"
+          async
+        />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/debug.addIndicators.min.js"
+          async
+        />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"
+          async
+        />
+        {/* <script src="lazysizes.min.js" async /> */}
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
